abff5514b5a40bed0fedca529a901904f71220dc
colmccaffrey <colmccaffrey@users.noreply.github.com>
2018-02-25 20:20:51 -0500

    updated project to prototype with d3.js

--- project_1/met-quant/src/sketch.js
+++ project_1/met-quant/src/sketch.js
@@ -1,129 +1,93 @@
+var selectionYear = 0;
 
-var slider;
-var selectedYear;
-var count;
-var objectDate;
-var dept;
-var year;
-var table;
-var objectYear;
-var yearAcquired;
-var totalArray = [];
+d3.csv("data/csvCut_MetObjects_5000.csv", function(d) {
+  return {
+    acquired : checkYear(d.Acquired), 
+    created : +d.Created,
+    dept : checkDept(d.Department)
+  };
+}, function(data) {
 
-function preload() {
- table = loadTable('data/csvCut_MetObjects_5000.csv', 'csv', 'header');
-}
+	var slider2 = d3.sliderHorizontal()
+    .min(1871)
+    .max(2017)
+    .step(1)
+    .width(800)
+    .on('onchange', val => {
+    	d3.select("p#value2").text(val);
+    	selectionYear = val;
+    	//selectionYearSlider(val);
+    	//console.log("data[0].acquired" + data[0].acquired +  " selectionYear " + selectionYearSlider());
 
-function setup() {
- createCanvas(1000, 600);
- slider = createSlider(1871, 2018, 1871);
- slider.position(512, 700); 
-  text('< 1500',300,560);
-  text('1600',500,560);
-  text('1700',600,560); 
-  text('1800',700,560);
-  text('1900',800,560);
-  text('2000',900,560);
 
-}
+});
 
-function draw() {
-  //background(127);
-  selectedYear = slider.value();
-  //console.log("slider year: " + selectedYear);
-  drawArcs();
- 	//drawAll();
-}
+  
+  console.log("slider value " + slider2.value());
 
-function drawArcs() {
- console.log("selected year " + selectedYear);
- count = table.getRowCount();
- var yearArray = [];
- for (var i = 0; i < count; i ++) {
- 	//console.log(count);
-		objectDate = Number(table.getString(i, 2));
-		dept = table.getString(i, 1);
-		year = table.getString(i, 0);
- 	 var acquiredYearString = checkYear(year);
- 	 var acquiredYear = Number(acquiredYearString);
-	 if (selectedYear == acquiredYear){ 
-	 	yearArray.push([objectDate, dept, acquiredYear],);
-	 	//console.log("year Array : " + yearArray);
-	 	}
-	 totalArray.push([objectDate, dept, acquiredYear]);
-	}
+  var g = d3.select("div#slider2").append("svg")
+    .attr("width", 500)
+    .attr("height", 100)
+    .append("g")
+    .attr("transform", "translate(30,30)");
 
-	for (var j=0; j < yearArray.length; j++) {
-		console.log("array lentghth " + yearArray.length);
-	  		stroke(Math.floor(Math.random()* 120), Math.floor(Math.random()*255), Math.floor(Math.random()*255)); //this needs a new variable to check for all objects acquired, what dept they were acquired by in that year, and the stroke of each object is defined by that variable- write function to itertate through
-	  		noFill();
-	  		var x;
-	  		 if (yearArray[j][0] <= 1500 ) {
-	  		 	x = 300;
-	  		 } else if (yearArray[j][0] <= 1600 ) {
-	  		 	x = 500;
-	  		 } else if (yearArray[j][0] <= 1700 ) {
-	  		 	x = 600;
-	  		 } else if (yearArray[j][0] <= 1800 ) {
-	  		 	x = 700; 
-	  		 } else if (yearArray[j][0] <= 1900 ) {
-	  		 	x = 800; 
-	  		 }else if (yearArray[j][0] <= 2018 ) {
-	  		 	x = 900; 
-	  		 } 
+  g.call(slider2);
 
-	  		 x = yearArray[j][0] * .01 + x;
-	  		 console.log(" x = " + x);
-	  		 var h = yearArray[j][2] - yearArray[j][0];
-	  		 var w = yearArray[j][2] - yearArray[j][0];
-			 arc(x , 550, w, h, PI, TWO_PI);
+  d3.select("p#value2").text((slider2.value()));
+  d3.select("a#setValue2").on("click", () => slider2.value());
 
-			//console.log(" h/w : acquired: " + yearArray[j][2] + " created: " + yearArray[j][0] + " h/w: " + h + " , x = " + x);
-			//arc(created[i] * 10, 250, acquiredYear - created[i] * 10, acquiredYear - created[i] * 10, PI, TWO_PI);
-	  		 //line(created[i] * 10, 250, acquiredYear[j] + 100, 150);
 
-			}
 
+for (var i = 0; i < data.length; i++ ) {
+if (!null) {
+var x1 = (data[i].created - 1000);
+var x2 = (data[i].acquired - 1000);
+var x = x2 - x1;
+var ySet = 575;
+var xTest =10 * i;
+
+var svg = d3.select("svg")
+.append("g")
+
+
+var arc = d3.arc()
+  
+  .innerRadius(x/2)
+  .outerRadius(x/2);
+
+
+var sector = svg.append("path")
+  .attr("fill", "none")
+  .attr("stroke-width", .2)
+  .attr("stroke", data[i].dept)
+  .on("mouseover", function(d, i){
+  	  		d3.select(this).style("stroke", "navy").attr("stroke-width", 5).attr("z-index", 5000);
+			d3.select("span#details").text(data[this].created);
+             })	
+  .on("mouseout", function(d,i) {
+  		d3.select(this).style("stroke", data[i].dept).attr("stroke-width", .2);
+  		d3.select("span#details").text("  "); 
+
+  	})
+
+  .attr("transform", function(d, i) {
+            // arc will always be drawn around (0, 0)
+            // shift so (0, 0) will be between created and acquired
+            if (x1 > 0) {
+	            var xshift = x1 + (x2 - x1) / 2;
+	            var yshift = ySet;
+	            return "translate(" + xshift + ", " + yshift + ")";
+	        } else {
+	        	xhift = (x1 * .001) / 2;
+	        	var yshift = ySet - 100;
+	            return "translate(" + xshift + ", " + yshift + ")";
+	        }
+        })
+  .attr("d", arc({startAngle:(-0.5 * Math.PI), endAngle:(0.5 * Math.PI)}))
 		}
-
-function checkDept(dept) {
-		
-		//assign fill color;
-
 	}
 
-
-function drawAll() { // option to have all arcs on the page
-
-	for (var j=0; j < totalArray.length; j++) {
-	  		stroke('yellow');//checkDept(dept)); //this needs a new variable to check for all objects acquired, what dept they were acquored by in that year, and the stroke of each object is defined by that variable- write function to itertate through
-	  		 noFill();
-	  		 var x;
-	  		
-	  		 if (totalArray[j][0] <= 1500 ) {
-	  		 	x = 50;
-	  		 } else if (totalArray[j][0] <= 1600 ) {
-	  		 	x = 100;
-	  		 } else if (totalArray[j][0]<= 1700 ) {
-	  		 	x = 200;
-	  		 } else if (totalArray[j][0] <= 1800 ) {
-	  		 	x = 300; 
-	  		 } else if (totalArray[j][0] <= 1900 ) {
-	  		 	x = 400; 
-	  		 } else if (totalArray[j][0] <= 2000 ) {
-	  		 	x = 500; 
-	  		 } else if (totalArray[j][0] > 2000 ) {
-	  		 	x = 600; 
-	  		 }
-
-	  		 //console.log(x);
-	  		 x = x + totalArray[j][0] * .01;
-	  		 console.log(" x " + x);
-	  		 var h = totalArray[j][2] - totalArray[j][0];
-	  		 var w = totalArray[j][2] - totalArray[j][0];
-			 arc(x , 350, w, h, PI, TWO_PI);
-	}
-}
+});
 
 function checkYear(x){
     objectYear = x.split(".", 1 );
@@ -141,76 +105,135 @@ function checkYear(x){
 		yearAcquired = null;
 		return yearAcquired;
 	}
-	//return yearAcquired; 
+
 }
-
-
-/* KEEP
- //line(created[i] * 10, 250, acquiredYear[j] + 100, 150);
- //arc(selectedYear + 50, 200, dept[i] - selectedYear PI, dept[i] + selectedYear, PI, TWO_PI);
-//arc(x,y,w,h,start,stop,[mode])
-
-// variables 
-
-//objects
- x = year made
- y= y -1 (increment for each  item, sort on y axis lphabetically, on x axis by time)
-
-
- //arc
- arc = PI + TWO_PI
-
- x = year made
- y  = constant
- x start= year made
- x stop = year acquired
- height = x/2  (based on x)
- width = xstop - xstart
- stroke = dept cat
-	
- 
-
-********************************** KEEP ****************************** */
-
-
-	//how many depts are there? 20
-	//department, year created, year acquired- culture or country seem to be more where the work was created, doesn't necessarily match with artist nationality, either way these fields are only available in about 50% of object records
-   
-
 
 /*
-function checkYear(x){
-    var objectYear = x.split(".", 1 );
-	var yearAcquired = Number(objectYear);
-	//console.log("year acquired: " + objectYear);
-	if (yearAcquired >= 1900) {
-		//return yearAcquired;
-		yearAcquired = yearAcquired;
-		console.log("year acquired: " + yearAcquired );
-		allYears.push(yearAcquired);
-	} else if (yearAcquired <= 71) {
-		yearAcquired  = 1900 + yearAcquired;
-		//return yearAcquired;
-		console.log("year acquired: " + yearAcquired );
-				allYears.push(yearAcquired);
+function selectionYearSlider(){
+		return selectionYear;	
+}
 
-
-	} else if (yearAcquired  > 71 && yearAcquired  < 1900) {
-		yearAcquired  = 1800 + yearAcquired ;
-		//return yearAcquired;
-		console.log("year acquired: " + yearAcquired );
-
-				allYears.push(yearAcquired);
-
-
-	} else {
-		//return yearAcquired;
-		yearAcquired = -1;
-		console.log("year acquired: " + yearAcquired );
-	}
-
-
+function checkYearAcquired(d, selYear){	
+	d3.selectAll("path")
+		.styles("stroke-width", function (d, i){
+				console.log("selYear, d.acquired" + d.acquired);
+				if (selYear == d.acquired) {
+					return 2;
+				}else {
+					return 0.2;
+				}
+	})
 
 }
+*/
+
+
+
+function checkDept(e) {
+	if (e === "American Decorative Arts") {
+		return "#a6cee3";
+	} else if (e === "American Paintings and Sculpture") {
+		return "#1f78b4";
+	} else if (e === "Ancient Near Eastern Art") {
+		return "#b2df8a";
+
+	} else if (e === "Arms and Armor") {
+		return "#33a02c";
+
+	} else if (e === "Asian Art") {
+		return "#fb9a99";
+
+	} else if (e === "Costume Institute") {
+		return "#e31a1c";
+
+	} else if (e === "Drawings and Prints") {
+		return "#fdbf6f";
+
+	} else if (e === "Egyptian Art") {
+		return "#ff7f00";
+
+	} else if (e === "European Paintings") {
+		return "#cab2d6";
+
+	} else if (e === "European Sculpture and Decorative Arts") {
+		return "#6a3d9a";
+
+	} else if (e === "Greek and Roman Art") {
+		return "#ffff99";
+
+	} else if (e === "Islamic Art") {
+		return "#b15928";
+
+	} else if (e === "Medieval Art") {
+		return "#1b9e77";
+
+	} else if (e === "Modern and Contemporary Art") {
+		return "#d95f02";
+
+	} else if (e === "Musical Instruments") {
+		return "#7570b3";
+
+	} else if (e === "Photographs") {
+		return "#e7298a";
+
+	} else if (e === "Robert Lehman Collection") {
+		return "#66a61e";
+
+	} else if (e === "The Cloisters") {
+		return "#e6ab02";
+
+	} else if (e === "The Libraries") {
+		return "#a6761d";
+
+	} else {
+		return "#626262";
+
+	}
+}
+
+
+/* departments
+American Decorative Arts
+American Paintings and Sculpture
+Ancient Near Eastern Art
+Arms and Armor
+Asian Art
+Costume Institute
+Drawings and Prints
+Egyptian Art
+European Paintings
+European Sculpture and Decorative Arts
+Greek and Roman Art
+Islamic Art
+Medieval Art
+Modern and Contemporary Art
+Musical Instruments
+Photographs
+Robert Lehman Collection
+The Cloisters
+The Libraries
+"Arts of Africa, Oceania, and the Americas"
+*/
+/*color brewer generator
+#a6cee3
+#1f78b4
+#b2df8a
+#33a02c
+#fb9a99
+#e31a1c
+#fdbf6f
+#ff7f00
+#cab2d6
+#6a3d9a
+#ffff99
+#b15928
+#1b9e77
+#d95f02
+#7570b3
+#e7298a
+#66a61e
+#e6ab02
+#a6761d
+#666666
 */
 
