9d70d33f70bebc99639dbbb1ef8232096386d9d5
colmccaffrey <colmccaffrey@users.noreply.github.com>
2018-04-30 00:36:31 -0400

    updated path

--- m_project_2/src/assets/sketch.js
+++ m_project_2/src/assets/sketch.js
@@ -21,12 +21,12 @@ var activeCircle;
 
  
 function preload() {
-  var data = "/src/data/collectionlisting-thirty-six-views.json";
+  //var data = "/src/data/collectionlisting-thirty-six-views.json";
   //var data = "/src/data/collectionlisting-thirty-six-views.json";  //change url when add to github
-  //var data = 'https://colmccaffrey.github.io/DV5200/project_2/src/data/collectionlisting-thirty-six-views.json';
+  var data = 'https://colmccaffrey.github.io/DV5200/project_2/src/data/collectionlisting-thirty-six-views.json';
   images = loadJSON(data);
   //console.log(images);
-  var gdata = "/src/data/street-view-gallery.json"
+  var gdata = "https://colmccaffrey.github.io/DV5200/project_2/src/data/street-view-gallery.json"
   gviews = loadJSON(gdata);
   console.log(gviews);
 
